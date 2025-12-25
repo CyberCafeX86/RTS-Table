cb1bf885d6a0fa7cf46249e8d91d96fd4d18db52
CyberCafeX86 <cybercafe_x86@keemail.me>
2025-12-25 12:43:38 -0300

    Better commentaries and allocated to Fun + Games entities section

--- lua/entities/RTS Table/init.lua
+++ lua/entities/RTS Table/init.lua
@@ -5,10 +5,11 @@ include("shared.lua")
 
 -- Server-side initialization function for the Entity
 function ENT:Initialize()
-    self:SetModel( "models/hunter/blocks/cube025x025x025.mdl" ) -- Sets the model for the Entity.
+    self:SetModel( "models/props_wasteland/kitchen_counter001d.mdl" ) -- Sets the model for the Entity.
     self:PhysicsInit( SOLID_VPHYSICS ) -- Initializes physics for the Entity, making it solid and interactable.
     self:SetMoveType( MOVETYPE_VPHYSICS ) -- Sets how the Entity moves, using physics.
     self:SetSolid( SOLID_VPHYSICS ) -- Makes the Entity solid, allowing for collisions.
+    
     local phys = self:GetPhysicsObject() -- Retrieves the physics object of the Entity.
     if phys:IsValid() then -- Checks if the physics object is valid.
         phys:Wake() -- Activates the physics object, making the Entity subject to physics (gravity, collisions, etc.).
